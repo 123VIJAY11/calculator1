607a5028eae6104d740919a3b2c7a5aef5dff715
123VIJAY11 <rv981553@gmail.com>
2023-05-01 18:04:45 +0530

    second commit

--- app/src/main/java/com/example/calculator1/MainActivity.java
+++ app/src/main/java/com/example/calculator1/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.calculator1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn7,btndelete;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn7 = findViewById(R.id.btn7);
+        btndelete = findViewById(R.id.btndelete);
+        textView = findViewById(R.id.txt);
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("7");
+            }
+        });
+
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("0");
+            }
+        });
+
+
+
+
     }
 }
